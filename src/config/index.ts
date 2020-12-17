5c305d6159653d0feaed5446c943530582b63978
Yerzhan Karatayev <erzhan.kst@gmail.com>
2020-12-17 15:10:14 +0900

    updated tailwindcss settings, added postcss, webpack configs. updated config order.

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,12 +5,12 @@ export interface AppConfig {
 
 let AppConfig: () => AppConfig;
 
-if (process.env.REACT_APP_ENV === 'local') {
-  AppConfig = require('./config.local').default;
+if (process.env.REACT_APP_ENV === 'stg') {
+  AppConfig = require('./config.stg').default;
 } else if (process.env.REACT_APP_ENV === 'prod') {
   AppConfig = require('./config.prod').default;
 } else {
-  AppConfig = require('./config.stg').default;
+  AppConfig = require('./config.local').default;
 }
 
 export default AppConfig();
